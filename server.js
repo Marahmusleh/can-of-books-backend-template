ec3af21672d796d15b631ee9ed79df92104f1fb2
marahmusleh <marahaltarefe18@gmail.com>
2021-08-09 01:23:13 +0300

    data from backend

--- server.js
+++ server.js
@@ -1,24 +1,49 @@
 'use strict';
 
-require('dotenv').config();
 const express = require('express');
+require('dotenv').config();
 const cors = require('cors');
-const jwt = require('jsonwebtoken');
-const jwksClient = require('jwks-rsa');
-
 const app = express();
-app.use(cors());
+const jwt = require('jsonwebtoken');
+const jwksClient = require('jwks-rsa'); // we are going to use this package to connect to Auth0
+const PORT = process.env.PORT;
+const JWKSURI = process.env.JWKSURI;
+app.use(cors())
 
-const PORT = process.env.PORT || 3001;
+const client = jwksClient({
+  jwksUri: JWKSURI
+});
+
+
+
+function getKey(header, callback){
+  client.getSigningKey(header.kid, function(err, key) {
+    var signingKey = key.publicKey || key.rsaPublicKey;
+    callback(null, signingKey);
+  });
+}
+ 
+
 
 app.get('/test', (request, response) => {
+const token = request.headers.authorization.split(' ')[1]; // take the token from frontend
+jwt.verify(token, getKey, {}, (error, user) =>{ // pass it to the auth to check the token if its valid
+  if(error){
+    response.send('invalid token');
+  }
+  response.json(user);//send user information in the state from auth
+});
+});
 
+ 
   // TODO: 
   // STEP 1: get the jwt from the headers
   // STEP 2. use the jsonwebtoken library to verify that it is a valid jwt
   // jsonwebtoken dock - https://www.npmjs.com/package/jsonwebtoken
   // STEP 3: to prove that everything is working correctly, send the opened jwt back to the front-end
 
-})
 
-app.listen(PORT, () => console.log(`listening on ${PORT}`));
+
+  app.listen(PORT, () => {
+    console.log(`Server started on ${PORT}`);
+  });
